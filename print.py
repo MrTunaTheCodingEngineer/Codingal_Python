08c1134c79a571117e5fdfe308de3cca4b7cf203
MrTunaTheCodingEngineer <alitunapalta@icloud.com>
2025-04-24 18:35:46 +0200

    Add files via upload

--- print.py
+++ print.py
@@ -1,7 +1,34 @@
+import keyword
+
+print(keyword.kwlist)
 print("Ali")
 print("Codingal")
 print("Basketball")
 print("Lion")
 
+#print Senctence
+print("Welcome to Codingal")
+
 #print numbers
 print(123)
+
+print("Swathi\n")
+print("Ali")
+#Next line after Print
+print("Hello Python \n")
+
+#Pass multiple  arguments for printing
+print("Hello",5)
+
+#printing different variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+#User Input
+
+Name = input ("enter your name:")
+
+print("\nHello", Name,"\nWelcome to Codingal")
+
