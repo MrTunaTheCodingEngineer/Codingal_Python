75068851927afd25fccedfa609619ab6a9b30c4e
MrTunaTheCodingEngineer <alitunapalta@icloud.com>
2025-05-01 18:38:12 +0200

    Add files via upload

---
+++ percentage.py
@@ -0,0 +1,13 @@
+print("enter all your test results")
+Math = int(input("Math: "))
+English = int(input("English: "))
+Science = int(input("Science: "))
+German = int(input("German: "))
+
+sum = Math+English+Science+German
+print("Percantage of the marks: ")
+
+perc = (sum/400)*100
+
+print(perc)
+
