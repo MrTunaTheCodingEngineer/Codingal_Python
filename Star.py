f59b89bf6dffd06624a70cdb3f3296336659eef9
MrTunaTheCodingEngineer <alitunapalta@icloud.com>
2025-05-29 18:46:04 +0200

    Add files via upload

---
+++ Star.py
@@ -0,0 +1,15 @@
+print("Half pyramid of stars (*)")
+n = int(input("Enter how many rows tou want to have: "))
+
+for i in range(n):
+
+    for j in range(i+1):
+         
+         print("* ", end = "")
+    print()
+        
+        
+            
+
+
+       
