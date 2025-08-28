c995836f97a2999f16884317ab2dd94ff11bb040
MrTunaTheCodingEngineer <alitunapalta@icloud.com>
2025-08-28 18:39:24 +0200

    Add files via upload

---
+++ Ontime.py
@@ -0,0 +1,10 @@
+def Ontime(n):
+    iteration = 0
+    for i in range(1, n+1):
+        iteration += 1
+    print("when 'n' is ",n,", Iterations = ", iteration)
+
+Ontime(10)
+Ontime(20)
+
+print("\nWith every 'n' the time taken and iterations will increase")
---
+++ iteration.py
@@ -0,0 +1,12 @@
+def printnumber(n):
+    iteration = 0
+    print("Number entered by user is: ", n)
+    iteration += 1
+    print("Total iteration done by code is: ", iteration, "\n")
+
+printnumber(10)
+printnumber(20)
+printnumber(40)
+printnumber(1000)
+print("\n with any 'n' the time taken by our code wont change")
+
