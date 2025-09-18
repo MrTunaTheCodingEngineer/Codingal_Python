9d5f8d0bd906a6ae3a785bc53d758c3f6d3b1a90
MrTunaTheCodingEngineer <alitunapalta@icloud.com>
2025-09-18 18:38:31 +0200

    Add files via upload

---
+++ Tuple.py
@@ -0,0 +1,20 @@
+tuple = ("Tuples", False, 3.2,4)
+print(tuple)
+
+tuple = (2,3,4,1,7,8,5)
+print(tuple)
+
+tuple = tuple +(20,)
+print(tuple)
+
+
+tuple1 = (10, 20, 40, 30, 70)
+print(tuple1.count(10))
+
+tuple = (2, 4, 2, 5, 3, 1 ,6 ,7, 9)
+_slice = tuple[2:7]
+print(_slice)
+
+_slice = tuple[:3]
+
+print(_slice)
