263ba7e5ff03efbbf772dede7ee630fc70bbc6fe
MrTunaTheCodingEngineer <alitunapalta@icloud.com>
2025-09-19 21:31:38 +0200

    Add files via upload

---
+++ Tuples.py
@@ -0,0 +1,19 @@
+tupley = ("python" ,2314, True)
+print(tupley)
+
+tupley = (2,4,1,6,8,3,6,8)
+print(tupley)
+
+tupley = tupley +(1203,)
+print(tupley)
+
+tuplez = (230,456,198,354,632)
+print(tuplez.count(230))
+
+tuple = (2,8,4,7,5,9,3)
+_slice = tuple[8:9]
+print(_slice)
+
+_slice = tuple[:7]
+
+print(_slice)
