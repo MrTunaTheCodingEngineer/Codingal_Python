af3eb49c3a1fc8d38b3e4612ae382cd4bb7f3c55
MrTunaTheCodingEngineer <alitunapalta@icloud.com>
2025-09-20 22:32:29 +0200

    Add files via upload

---
+++ isitalphabet.py
@@ -0,0 +1,30 @@
+alpha = "f"
+beta = "12"
+gamma = "r"
+
+print(alpha != beta)
+print(gamma != beta)
+
+delta = "7"
+epsilon = "e"
+
+print(delta, "and", epsilon, "are not the same")
+
+
+zeta = "z"
+eta = "4"
+
+if (zeta == "c") != (eta == "4"):
+    print("Python")
+
+
+    theta = int(input("enter a number: "))
+
+    if theta%2 != 0 :
+        print("This number is a odd number")
+
+  
+
+
+
+
