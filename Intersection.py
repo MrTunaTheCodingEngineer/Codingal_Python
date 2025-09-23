814e8a3201bcde4855dcc9765f01eceffac4db63
MrTunaTheCodingEngineer <alitunapalta@icloud.com>
2025-09-23 19:39:11 +0200

    Add files via upload

---
+++ Intersection.py
@@ -0,0 +1,8 @@
+setx = {"Mustafa", "Muhammed", "Kayra"}
+sety = {"Umut", "ömer","Kayra", "Gökmen"}
+print("The Original set elements: ")
+print(setx)
+print(sety)
+print("\nIntersection of the said elements")
+setz = setx.intersection(sety)
+print(setz)
---
+++ sets.py
@@ -0,0 +1,18 @@
+my_set = {2398,219,644}
+print(my_set)
+
+my_set = {26.4 ,"Astonishing" ,(6,2,7)}
+print(my_set)
+
+my_set = {2,5,4,4,6,2,2,1,7,7}
+print(my_set)
+
+my_set = set([5,2,3,1])
+print(my_set,"\n")
+
+num_set = set([0,4,7,8,5])
+print("Original Set: ", num_set)
+num_set.pop()
+print("After moving the first element from the said set: ")
+print(num_set,"\n")
+
